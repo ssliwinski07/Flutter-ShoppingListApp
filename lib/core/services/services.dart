1401c3edd1c86eab523dc978c4d74eaa6bac47aa
Szymon Śliwiński <122596933+ssliwinski07@users.noreply.github.com>
2024-02-17 16:25:32 +0100

    folder restructurization - part I

---
+++ lib/core/services/services.dart
@@ -0,0 +1,3 @@
+export 'hive_service/hive_service.dart';
+export 'message_info_service/message_service_info.dart';
+export 'services_locator/service_locator.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_reminder/core/services/hive_service/hive_service.dart';
-import 'package:shopping_reminder/core/services/services_locator/service_locator.dart';
+import 'core/core.dart';
 import 'package:shopping_reminder/mobx/stores/shopping_items_store.dart';
 import 'package:shopping_reminder/views/main_screen_view/main_screen_view.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
